1e2941af6ee77fdb41bbab26e5e378c9a6cc891d
gracetechnologies <appfitkare@gmail.com>
2023-11-23 16:09:59 +0500

    Update 2023_05_16_123255_create_tokens_table.php

--- database/migrations/2023_05_16_123255_create_tokens_table.php
+++ database/migrations/2023_05_16_123255_create_tokens_table.php
@@ -23,8 +23,8 @@ return new class extends Migration
     {
         Schema::create('tokens', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('category_id');
-            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+            // $table->unsignedBigInteger('category_id');
+            // $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
             $table->text('token');
             $table->timestamps();
             $table->softDeletes();
